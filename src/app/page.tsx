a44962b5864fd8047dd5928ae37c9f17d3923874
Johnravee <johnravemimayn@gmail.com>
2025-01-15 11:48:20 +0800

    modified avatar div, added socials icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import * as React from "react";
 import ModeToggle from "@/components/ui/ModeToggle";
 import About from "@/components/ui/about";
 import Certificates from "@/components/ui/certificates";
@@ -8,25 +7,40 @@ import TechStacks from "@/components/ui/teck-stacks";
 import Connects from "@/components/ui/connects";
 import Works from "@/components/ui/works";
 import Image from 'next/image'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-auto w-full mb-5 px-4">
-      <div className="light h-16 w-full max-w-screen-lg flex justify-end items-center mb-4">
+    <div className="flex flex-col items-center justify-center h-auto w-full  px-4">
+      <div className="light h-16 w-full max-w-screen-lg flex justify-end items-center mb-2">
         <ModeToggle />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-7 gap-4 w-full max-w-screen-lg">
         
         {/* Avatar Section */}
-        <div className="col-span-1 sm:col-span-2 lg:col-span-2 row-span-4 w-full h-40 rounded-lg bg-gray-400 dark:bg-gray-600 flex justify-end items-end">
-          <Image
-            src="/me.jpg"
-            className="rounded-lg w-full h-full bg-cover"
-            alt="Avatar"
-            width={500}
-            height={500}
-          />
+       <div className="col-span-1 sm:col-span-2 lg:col-span-2 row-span-4 w-full h-40 rounded-lg bg-white dark:bg-[#1111] border flex justify-end items-end overflow-hidden">
+          <div className="h-full w-56">
+            <Image
+              src="/me.jpg"
+              className="w-full h-full object-cover"
+              alt="Avatar"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div className="flex w-20 h-full justify-center items-center flex-col gap-5">
+            <Link href="https://github.com/Johnravee" target="_blank" className="hover:text-gray-400"><FaGithub size={18} /></Link>
+            <Link href="https://www.linkedin.com/in/john-rave-mimay-740a06237/" className="hover:text-gray-400"><FaLinkedin size={18} /></Link>
+            <Link href="https://www.facebook.com/johnraveee/" className="hover:text-gray-400"><FaFacebook size={18} /></Link>
+            <Link href="https://medium.com/@johnravemimayn" className="hover:text-gray-400"><FaMedium size={18} /></Link>
+          </div>
         </div>
+
+
         {/* About Section */}
        <div className="col-span-1 sm:col-span-2 lg:col-span-5 row-span-6 min-h-[300px] bg-white border rounded-lg dark:bg-[#1111]  overflow-hidden">
           <About />
