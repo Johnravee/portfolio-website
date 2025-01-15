5ef9ec4fcca973a17135113c8892ff96e00eb6cc
Johnravee <johnravemimayn@gmail.com>
2025-01-16 00:39:10 +0800

    added own website logo and change tab title

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['imgs.search.brave.com'], // Add the allowed external image domain here
-  },
+
 };
 
 export default nextConfig;
